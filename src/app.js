e4610ca9bd9cfb479545caf77963fcb3b8b35dc9
Nguyen-Van-Chinh <Nguyen.VanChinh@coxautoinc.com>
2021-08-28 20:24:47 +0700

    set app for deploy to heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -53,7 +54,7 @@ app.get('/weather', (req, res) => {
         latitude,
         longitude,
         location
-    }={}) => {
+    } = {}) => {
         if (error) {
             return res.send({
                 error
@@ -102,6 +103,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log(chalk.green.inverse('Server is up on port 3000!!'))
+app.listen(port, () => {
+    console.log(chalk.green.inverse('Server is up on port ' + port))
 })
